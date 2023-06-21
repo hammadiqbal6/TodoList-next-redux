228f37d64652ed9ca04aee4314292dc1f07fff39
Hammad Iqbal <hammad.iqbal@RT-LT-555.rolustech.net>
2023-06-21 16:17:22 +0500

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import TodoList from "@/components/TodoList";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,21 +1,41 @@
 "use client";
-import { useAppSelector } from "@/redux/hooks";
+// import { useAppSelector } from "@/redux/hooks";
+import { RootState } from "../redux/store";
 import React from "react";
 import TodoItem from "./TodoItem";
+import { createSelector } from "reselect";
+import { useSelector } from "react-redux";
 
 function TodoList() {
-  const todoList: Todo[] | [] = useAppSelector((state) => {
-    switch (state.visibilityFilter) {
-      case "all":
-        return state.todo;
-      case "active":
-        return state.todo.filter((todo) => !todo.completed);
-      case "completed":
-        return state.todo.filter((todo) => todo.completed);
-      default:
-        return [];
+  // const todoList: Todo[] | [] = useAppSelector((state) => {
+  //   switch (state.visibilityFilter) {
+  //     case "all":
+  //       return state.todo;
+  //     case "active":
+  //       return state.todo.filter((todo) => !todo.completed);
+  //     case "completed":
+  //       return state.todo.filter((todo) => todo.completed);
+  //     default:
+  //       return [];
+  //   }
+  // });
+  const selectNumCompletedTodos = createSelector(
+    (state: RootState) => state,
+    ({ todo, visibilityFilter }) => {
+      switch (visibilityFilter) {
+        case "all":
+          return todo;
+        case "active":
+          return todo.filter((todo) => !todo.completed);
+        case "completed":
+          return todo.filter((todo) => todo.completed);
+        default:
+          return [];
+      }
     }
-  });
+  );
+  const todoList: Todo[] = useSelector(selectNumCompletedTodos);
+
   return (
     <div className="flex flex-col gap-y-4 mt-3" data-testid="todoList-element">
       {todoList.map((todo) => (
